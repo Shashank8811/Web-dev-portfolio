0bdb7529479dc30bc7f8c85598516a2d52082341
Shashank88 <shashankreddy1817@gmail.com>
2024-03-19 16:39:48 +0530

    major changes

--- script.js
+++ script.js
@@ -4,17 +4,3 @@ const navEl = document.querySelector(".header");
 btnEl.addEventListener("click", function () {
   navEl.classList.toggle("nav-open");
 });
-
-let i = 0;
-let txt =
-  "Hello, I'm SHASHANK a junior front end developer on a mission to create user-friendly, efficient and responsive websites.";
-const speed = 30;
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("text").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
-typeWriter();
