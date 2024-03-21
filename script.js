32a5ca37ad23ee8b7829d0e39353dd30a6ba087f
Shashank88 <shashankreddy1817@gmail.com>
2024-03-21 09:12:09 +0530

    Update

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const navEl = document.querySelector(".header");
 btnEl.addEventListener("click", function () {
   navEl.classList.toggle("nav-open");
 });
+
+let formContent = document.querySelector(".form-contact");
+
+let subBtn = document.getElementById("submit");
+
+subBtn.addEventListener("click", function () {
+  formContent.innerHTML = "<h1>Thanks I will get back to you soon.</h1>";
+});
